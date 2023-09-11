91ba535b1adfb1f49a6c039cce3503ee31f7358e
EdgarMnatsakanyan <143736950+EdgarMnatsakanyan@users.noreply.github.com>
2023-09-11 12:34:22 +0400

    Update EditName.jsx

--- src/components/UI/editname/EditName.jsx
+++ src/components/UI/editname/EditName.jsx
@@ -1,15 +1,10 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import cl from './EditName.module.css'
 import { RxCross2 } from 'react-icons/rx'
 import { useTranslation } from 'react-i18next';
 
 const EditName = ({visible, setVisible}) => {
     const { t } = useTranslation();
-    const data = useRef()
-    const add = () => {
-        console.log(data.current.value, 'initial value')
-        localStorage.setItem("inputValue", data.current.value)
-    }
 
     const rootClasses = [cl.editContiner]
     if(visible) {
@@ -25,7 +20,7 @@ const EditName = ({visible, setVisible}) => {
                 <hr />
                 <div className={cl.btnSection}>
                     <input type="text" placeholder='Full name/ Company name*'/>
-                    <button onClick={add}>{t("modalWindow.editnmae.button")}</button>
+                    <button onClick={() => setVisible(false)}>{t("modalWindow.editnmae.button")}</button>
                 </div>
             </div>
         </div>
